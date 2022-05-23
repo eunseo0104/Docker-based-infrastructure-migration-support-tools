f46c9a0e3aa95c547675d1a00a2a7c004b0df177
JongGeun Park <53558710+jonggeunpark@users.noreply.github.com>
2022-05-23 17:19:15 +0900

    Update App.java

--- aws2ncp/app/src/main/java/docker/App.java
+++ aws2ncp/app/src/main/java/docker/App.java
@@ -5,10 +5,7 @@ import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ecr.AmazonECR;
 
 import java.io.Console;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
     public static void main(String args[]) throws Exception {
@@ -31,6 +28,9 @@ public class App {
         boolean flag2 = false; // 타겟 클라우드 입력
 
 
+        printLogo();
+
+        /*
         System.out.println("\n" +
                 "            _                 _             \n" +
                 "           (_)               | |            \n" +
@@ -41,10 +41,15 @@ public class App {
                 "               __/ |                        \n" +
                 "              |___/                         \n");
 
-
-        System.out.println("Supported Cloud Platform List");
+        */
 
         Platform[] platforms = Platform.values();
+
+
+        printCloudPlatformList(platforms);
+        /*
+        System.out.println("Supported Cloud Platform List");
+
 
         System.out.println("-----------------------------");
         for(Platform p : platforms) {
@@ -53,8 +58,11 @@ public class App {
             System.out.println("            |");
         }
         System.out.println("-----------------------------");
+        */
 
-        String basedCloudName = "";
+
+        String basedCloudName = getCloudPlatformInput(scanner, platforms);
+        /*
         String targetCloudName = "";
 
         // base cloud platform 입력
@@ -72,30 +80,90 @@ public class App {
                 System.out.println("Invalid input");
             }
         }
+        */
 
+        String targetCloudName = getCloudPlatformInput(scanner, platforms);
+        /*
         // target Cloud Platform 입력
         while(!flag2) {
             // target cloud platform 입력
             System.out.println("Enter target cloud platform : ");
-            String targetCloud = scanner.nextLine();
+            int targetCloudNumber = scanner.nextLine();
 
-            // 입력에 해당하는 값이 cloudplatformlist 에 있다면 (유효한 입력이라면)
+            // 올바른 입력 ( platformList index )
             try {
                 targetCloudName = Platform.valueOf(targetCloud).name();
+                targetCloudName = platforms[targetCloudNumber].name();
                 flag2 = true;
             } catch(IllegalArgumentException e) {
                 System.out.println("Invalid Input");
             }
         }
+        */
 
         if(!migrator.migrate(basedCloudName, targetCloudName))
             System.out.println("Invalid Platform");
 
 
-
-
         String naverAccessKey = "peEjtbF0CEY4e8xN9fgi";
         String naverSecretKey = "gm2dEU9I8bjRGVshHxMFhWEpxmtj0VX2ecmdwn3A";
-
     }
+
+
+    // 로고 출력
+    private static void printLogo() {
+
+        System.out.println("\n" +
+                "            _                 _             \n" +
+                "           (_)               | |            \n" +
+                "  _ __ ___  _  __ _ _ __ __ _| |_ ___  _ __ \n" +
+                " | '_ ` _ \\| |/ _` | '__/ _` | __/ _ \\| '__|\n" +
+                " | | | | | | | (_| | | | (_| | || (_) | |   \n" +
+                " |_| |_| |_|_|\\__, |_|  \\__,_|\\__\\___/|_|   \n" +
+                "               __/ |                        \n" +
+                "              |___/                         \n");
+    }
+
+    // 플랫폼 리스트 출력
+    private static void printCloudPlatformList(Platform[] platforms) {
+
+        System.out.println("Supported Cloud Platform List");
+
+
+        System.out.println("-----------------------------");
+        for(int i=0; i<platforms.length; i++) {
+            System.out.print("|            ");
+            System.out.print(i + " : " + platforms[i].name());
+            System.out.println("            |");
+        }
+        System.out.println("-----------------------------");
+    }
+
+    // cloud platform 입력
+    private static String getCloudPlatformInput(Scanner scanner, Platform[] platforms) {
+
+        String cloudName = "";
+
+        // cloud platform 입력
+        while(true) {
+
+            // cloud platform 입력
+            System.out.println("Enter based cloud platform number : ");
+            int index = Integer.parseInt(scanner.nextLine());
+
+            // 입력에 해당하는 값이 cloudplatformlist 에 있다면 (유효한 입력이라면)
+            try {
+                cloudName = platforms[index].name();
+                return cloudName;
+
+                // 유효하지 않은 입력이라면
+            } catch(IllegalArgumentException | InputMismatchException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("유효하지 않은 입력입니다. 다시 입력해주세요.");
+            }
+        }
+    }
+
+    //
+
+
 }
