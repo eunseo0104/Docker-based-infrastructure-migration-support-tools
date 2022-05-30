c58155ac7ab79e7589123bfd6aada994a119036e
JongGeun Park <53558710+jonggeunpark@users.noreply.github.com>
2022-05-30 17:38:37 +0900

    Update Migrator.java

--- aws2ncp/app/src/main/java/docker/Migrator.java
+++ aws2ncp/app/src/main/java/docker/Migrator.java
@@ -55,9 +55,10 @@ public class Migrator {
 
         String region = getAwsRegionInput(regions);
 
-        AmazonEC2 amazonEC2Client = null;
         
-	// 2. 사용자로부터 awsAccessKey, awsSecretKey 입력 받음
+    	// 2. base Key 입력
+        AmazonEC2 amazonEC2Client = null;
+
         while(amazonEC2Client == null) {
 
             // aws 키 입력
@@ -68,74 +69,93 @@ public class Migrator {
             amazonEC2Client = buildEc2Client(awsKeyPair.getAccessKey(), awsKeyPair.getSecretKey(), region);
         }
 
-        // 3. 입력 받은 계정 정보로 해당 region의 모든 Instance 정보를 가져옴
-        List<Instance> instanceList = awsMigrator.getEC2InstanceDetails(amazonEC2Client);
-        System.out.println(instanceList);
-
-        // 4. 사용자에게 Instance 목록을 보여주고 이전할 Instance id를 입력 받음
-
-        printAwsInstanceIdList(instanceList);
-        
-        String instanceId = getAwsInstanceIdInput(instanceList);
-
-
-        List<String> instanceIds = new ArrayList<>();
-        instanceIds.add(instanceId);
-	
-	// 5.target key 입력
+        // 3.target key 입력
         KeyPair ncpKeyPair = getNcpKeyInput();
         ncpAccessKey = ncpKeyPair.getAccessKey();
         ncpSecretKey = ncpKeyPair.getSecretKey();
 
-        //6. 해당 Instance 서버 정보를 호출
-        List<Instance> selectedInstanceList = awsMigrator.getEC2InstanceDetails(amazonEC2Client, instanceIds);
+        System.out.println("Are you sure you want to migrate EC2 Instance?(Y/N): ");
+        while(true){
+            String answer = scanner.nextLine();
+            if(answer.equals("Y")){
+
+                // 3. 입력 받은 계정 정보로 해당 region의 모든 Instance 정보를 가져옴
+                List<Instance> instanceList = awsMigrator.getEC2InstanceDetails(amazonEC2Client);
+                System.out.println(instanceList);
+
+                // 4. 사용자에게 Instance 목록을 보여주고 이전할 Instance id를 입력 받음
+
+                printAwsInstanceIdList(instanceList);
+
+                String instanceId = getAwsInstanceIdInput(instanceList);
 
 
-        //7. 호출한 서버 정보를 공통 서버 정보로 변환
-        List<ServerInfo> serverInfoList = awsMigrator.convertEC2InstanceDetailsToServerInfo(amazonEC2Client, selectedInstanceList, Regions.valueOf(region));
+                List<String> instanceIds = new ArrayList<>();
+                instanceIds.add(instanceId);
 
 
-        List<String> ncpRegionList = ncpMigrator.getRegionList(ncpAccessKey, ncpSecretKey);
-
-        // target platform region list 출력
-        printNcpRegionList(ncpRegionList);
-
-        // target platform region 선택
-        String regionCode = getNcpRegionCodeInput(ncpRegionList);
+                //6. 해당 Instance 서버 정보를 호출
+                List<Instance> selectedInstanceList = awsMigrator.getEC2InstanceDetails(amazonEC2Client, instanceIds);
 
 
-        List<NcpServerImageProduct> ncpServerImageProductList = ncpMigrator.getNcpServerImageProductList(ncpAccessKey, ncpSecretKey, regionCode);
+                //7. 호출한 서버 정보를 공통 서버 정보로 변환
+                List<ServerInfo> serverInfoList = awsMigrator.convertEC2InstanceDetailsToServerInfo(amazonEC2Client, selectedInstanceList, Regions.valueOf(region));
 
-        // server image product list 출력
-        printServerImageProductList(ncpServerImageProductList);
 
-        // server image product 선택
-        NcpServerImageProduct ncpServerImageProduct = getNcpServerImageProductInput(ncpServerImageProductList);
+                List<String> ncpRegionList = ncpMigrator.getRegionList(ncpAccessKey, ncpSecretKey);
 
-        List<NcpServerProduct> serverProductList = ncpMigrator.getNcpServerProductList(ncpAccessKey, ncpSecretKey, regionCode, ncpServerImageProduct.getProductCode());
+                // target platform region list 출력
+                printNcpRegionList(ncpRegionList);
 
-        // server image product 에 적용 가능한 server product list 출력
-        printServerProductList(serverProductList);
+                // target platform region 선택
+                String regionCode = getNcpRegionCodeInput(ncpRegionList);
 
-        // server product 선택
-        NcpServerProduct ncpServerProduct = getNcpServerProductInput(serverProductList);
 
-        //8. 공통 서버 정보를 이전할 서버 정보로 변환
-        List<NcpServerInfo> ncpServerInfoList = new ArrayList<>();
+                List<NcpServerImageProduct> ncpServerImageProductList = ncpMigrator.getNcpServerImageProductList(ncpAccessKey, ncpSecretKey, regionCode);
 
-        for(ServerInfo serverInfo: serverInfoList) {
-            NcpServerInfo ncpServerInfo = ncpMigrator.convertServerInfoToNCPServerInfo(ncpAccessKey, ncpSecretKey, serverInfo, regionCode);
-            ncpServerInfo.serverImageProductCode = ncpServerImageProduct.getProductCode();
-            ncpServerInfo.serverProductCode = ncpServerProduct.getProductCode();
-            ncpServerInfoList.add(ncpServerInfo);
+                // server image product list 출력
+                printServerImageProductList(ncpServerImageProductList);
+
+                // server image product 선택
+                NcpServerImageProduct ncpServerImageProduct = getNcpServerImageProductInput(ncpServerImageProductList);
+
+                List<NcpServerProduct> serverProductList = ncpMigrator.getNcpServerProductList(ncpAccessKey, ncpSecretKey, regionCode, ncpServerImageProduct.getProductCode());
+
+                // server image product 에 적용 가능한 server product list 출력
+                printServerProductList(serverProductList);
+
+                // server product 선택
+                NcpServerProduct ncpServerProduct = getNcpServerProductInput(serverProductList);
+
+                //8. 공통 서버 정보를 이전할 서버 정보로 변환
+                List<NcpServerInfo> ncpServerInfoList = new ArrayList<>();
+
+                for(ServerInfo serverInfo: serverInfoList) {
+                    NcpServerInfo ncpServerInfo = ncpMigrator.convertServerInfoToNCPServerInfo(ncpAccessKey, ncpSecretKey, serverInfo, regionCode);
+                    ncpServerInfo.serverImageProductCode = ncpServerImageProduct.getProductCode();
+                    ncpServerInfo.serverProductCode = ncpServerProduct.getProductCode();
+                    ncpServerInfoList.add(ncpServerInfo);
+                }
+
+                //9. 서버 실행 및 완료 메시지 출력
+                for(NcpServerInfo ncpServerInfo: ncpServerInfoList) {
+                    ncpMigrator.runVPCInstance(ncpAccessKey, ncpSecretKey, ncpServerInfo);
+
+                    System.out.println(ncpServerInfo.serverName + "Migration completed");
+                }
+
+                System.out.println("Server Instance Migration Complete.");
+                break;
+            }
+            else if(answer.equals("N")){
+                System.out.println("Complete.");
+                break;
+            }
+            else{
+                System.out.println("(Y/N): ");
+            }
         }
 
-        //9. 서버 실행 및 완료 메시지 출력
-        for(NcpServerInfo ncpServerInfo: ncpServerInfoList) {
-            ncpMigrator.runVPCInstance(ncpAccessKey, ncpSecretKey, ncpServerInfo);
-
-            System.out.println(ncpServerInfo.serverName + "Migration completed");
-        }
         
         //10. Docker image Migration
         System.out.println("Are you sure you want to migrate AWS Docker image?(Y/N): ");
@@ -168,7 +188,7 @@ public class Migrator {
         }
 
         //11. Bucket Migration
-        System.out.println("Are you sure you want to migrate AWS Docker image?(Y/N): ");
+        System.out.println("Are you sure you want to migrate S3 Bucket?(Y/N): ");
         while(true){
             String answer = scanner.nextLine();
             if(answer.equals("Y")) {
@@ -185,7 +205,8 @@ public class Migrator {
                 String title = scanner.nextLine();
                 NcpMigrationJob ncpMigrationJob = new NcpMigrationJob(title, ncpMigrationJobSource, ncpMigrationJobTarget);
 
-                ncpObjectStorageMigrator.createMigrationJob(ncpAccessKey, ncpSecretKey, ncpMigrationJob);
+                ncpObjectStorageMigrator.createMigrationJob(ncpAccessKey, ncpSecretKey, ncpMigrationJob, region);
+                ncpObjectStorageMigrator.startMigrationJob(ncpAccessKey, ncpSecretKey, ncpMigrationJob, region);
             }
             else if(answer.equals("N")){
                 System.out.println("Complete.");
@@ -196,103 +217,38 @@ public class Migrator {
             }
 
         }
-
-
-
     }
 
     // ncp to aws 변환
     public void ncpToAws() throws Exception {
 
-        /*
-        boolean flag = false;
-        boolean flag2 = false;
-
-
-        Scanner scanner = new Scanner(System.in);
-        AwsMigrator awsMigrator = new AwsMigrator();
-        NcpMigrator ncpMigrator = new NcpMigrator();
-        */
-
-        String ncpAccessKey = null;
-        String ncpSecretKey = null;
+        String awsAccessKey = null;
+        String awsSecretKey = null;
         String ncpRegistryName = null;
         String awsRepositoryName = null;
-        String accessKey = null;
-        String secretKey = null;
 
-
+        // 1. 사용자로부터 ncpAccessKey, ncpSecretKey 입력 받음
         KeyPair ncpKeyPair = getNcpKeyInput();
-        ncpAccessKey = ncpKeyPair.getAccessKey();
-        ncpSecretKey = ncpKeyPair.getSecretKey();
-
-        /*
-        //1. 사용자로부터 ncpAccessKey, ncpSecretKey 입력 받음
-        while(!flag) {
-            flag = true;
-
-            System.out.println("Enter NCP Access Key : ");
-            ncpAccessKey = scanner.nextLine();
-            System.out.println(ncpAccessKey);
-
-            System.out.println("Enter NCP Secret Key : ");
-            ncpSecretKey = scanner.nextLine();
-            System.out.println(ncpSecretKey);
-
-            // 5-1. 자격 증명에 오류가 있는지 검사
-            if(!ncpMigrator.checkGetVPCList(ncpAccessKey, ncpSecretKey, "")) {
-                System.out.println("Invalid Key!");
-                flag = false;
-            }
-        }
-
-         */
+        String ncpAccessKey = ncpKeyPair.getAccessKey();
+        String ncpSecretKey = ncpKeyPair.getSecretKey();
 
         // 2. 입력 받은 계정 정보로 모든 InstanceNo 를 가져옴
         List<String> serverInstanceNoList = ncpMigrator.getServerInstanceNoList(ncpAccessKey, ncpSecretKey);
 
-
+        // 3. 모든 InstanceNo 출력 및 이전할 InstanceNo 입력
         printNcpInstanceNoList(serverInstanceNoList);
 
-        /*
-        // 3. 사용자에게 InstanceNo 목록을 보여주고 이전할 Instance no를 입력 받음
-        System.out.println("-----------------------------");
-        for (String serverInstanceNo : serverInstanceNoList) {
-            System.out.printf("|        %-18s |\n", serverInstanceNo);
-        }
-        System.out.println("-----------------------------");
-         */
-
         String instanceNo = getNcpInstanceNoInput(serverInstanceNoList);
-        /*
-        System.out.println("Enter Instance No : ");
-        String instanceNo = scanner.nextLine();
-        System.out.println(instanceNo);
-         */
 
         List<String> instanceNos = new ArrayList<>();
         instanceNos.add(instanceNo);
 
+        // 4. 타겟 플랫폼 region 리스트 출력 및 입력
         Regions[] regions = Regions.values();
         printAwsRegionList(regions);
-        /*
-        // 4. 사용자로부터 서버를 생성할 region을 입력 받음
-        System.out.println("-----------------------------");
-        Regions[] regions = Regions.values();
 
-        for (Regions r : regions) {
-            System.out.printf("|        %-18s |\n", r.name());
-        }
-        System.out.println("-----------------------------");
-         */
-
-
+        // 5. 타겟 플랫폼 키 입력 및 클라이언트 생성
         String region = getAwsRegionInput(regions);
-        /*
-        System.out.println("Enter Region : ");
-        String region = scanner.nextLine();
-        System.out.println(region);
-         */
 
         AmazonEC2 amazonEC2Client = null;
 
@@ -304,31 +260,6 @@ public class Migrator {
             // 키로 클라이언트 생성 및 검증
             amazonEC2Client = buildEc2Client(awsKeyPair.getAccessKey(), awsKeyPair.getSecretKey(), region);
         }
-
-        /*
-        // 5. 사용자로부터 awsAccessKey, awsSecretKey 입력 받음
-        AmazonEC2 amazonEC2Client = null;
-        while (!flag2) {
-            flag2 = true;
-
-            System.out.println("Enter AWS Access Key : ");
-            accessKey = scanner.nextLine();
-            System.out.println(accessKey);
-
-            System.out.println("Enter AWS Secret Key : ");
-            secretKey = scanner.nextLine();
-            System.out.println(secretKey);
-
-            // 입력 받은 정보로 ec2 client 생성
-            amazonEC2Client = awsMigrator.BuildEC2Client(accessKey, secretKey, Regions.valueOf(region));
-
-            // 자격 증명에 오류가 있는지 검사
-            if (!awsMigrator.checkGetEC2InstanceDetails(amazonEC2Client)) {
-                System.out.println("Invalid Key!");
-                flag2 = false;
-            }
-        }
-         */
 
         //6. 해당 Instance들의 정보를 공통 서버 정보로 변환
         List<ServerInfo> serverInfoList = ncpMigrator.convertNCPInstanceDetailsToServerInfo(ncpAccessKey,ncpSecretKey,instanceNos);
@@ -365,7 +296,7 @@ public class Migrator {
 			String awsAcountId = scanner.nextLine();
 			
 			String[] ncpInfo = {ncpRegistryName, ncpAccessKey, ncpSecretKey};
-			String[] awsInfo = {awsRepositoryName, accessKey, secretKey, region, awsAcountId};
+			String[] awsInfo = {awsRepositoryName, awsAccessKey, awsSecretKey, region, awsAcountId};
 			
 			ncpDockerMigrator.migrateDocker(ncpInfo, awsInfo);
 			System.out.println("Image Migration Complete.");
@@ -643,11 +574,11 @@ public class Migrator {
 
         System.out.println("Bucket List");
 
-        System.out.println("-------------------");
+        System.out.println("-----------------------------");
         for(int i=0; i<bucketList.size(); i++) {
-            System.out.printf("|      %-11s |\n", i + " : " + bucketList.get(i).getName());
+            System.out.printf("|      %-20s |\n", i + " : " + bucketList.get(i).getName());
         }
-        System.out.println("-------------------");
+        System.out.println("-----------------------------");
     }
 
     // NCP Region 입력
