f69a9dab6f4fb8c96c8e4765295f06347bbbfe0e
JongGeun Park <53558710+jonggeunpark@users.noreply.github.com>
2022-05-30 15:53:26 +0900

    Update App.java

--- aws2ncp/app/src/main/java/docker/App.java
+++ aws2ncp/app/src/main/java/docker/App.java
@@ -11,96 +11,19 @@ public class App {
     static Scanner scanner = new Scanner(System.in);
     static Migrator migrator = new Migrator();
 
-    static AwsMigrator awsMigrator = new AwsMigrator();
-    static NcpMigrator ncpMigrator = new NcpMigrator();
-
     public static void main(String args[]) throws Exception {
 
-        String awsAccessKey = "AKIA6CU5MHVS2ZQMCCNC";
-        String awsSecretKey = "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";
-        Regions regions = Regions.AP_NORTHEAST_2;
-
-        AmazonECR amazonECR = awsMigrator.BuildECRClient(awsAccessKey, awsSecretKey, regions);
-
-        ServerInfo serverInfo = new ServerInfo();
-
-        boolean flag1 = false; // 베이스 클라우드 입력
-        boolean flag2 = false; // 타겟 클라우드 입력
-
-
         printLogo();
-
-        /*
-        System.out.println("\n" +
-                "            _                 _             \n" +
-                "           (_)               | |            \n" +
-                "  _ __ ___  _  __ _ _ __ __ _| |_ ___  _ __ \n" +
-                " | '_ ` _ \\| |/ _` | '__/ _` | __/ _ \\| '__|\n" +
-                " | | | | | | | (_| | | | (_| | || (_) | |   \n" +
-                " |_| |_| |_|_|\\__, |_|  \\__,_|\\__\\___/|_|   \n" +
-                "               __/ |                        \n" +
-                "              |___/                         \n");
-
-        */
 
         Platform[] platforms = Platform.values();
 
         printCloudPlatformList(platforms);
-        /*
-        System.out.println("Supported Cloud Platform List");
-
-
-        System.out.println("-----------------------------");
-        for(Platform p : platforms) {
-            System.out.print("|            ");
-            System.out.print(p.name());
-            System.out.println("            |");
-        }
-        System.out.println("-----------------------------");
-        */
-
 
         System.out.println("Based Cloud Select");
         String basedCloudName = getCloudPlatformInput(platforms);
-        /*
-        String targetCloudName = "";
-
-        // base cloud platform 입력
-        while(!flag1) {
-
-            // base cloud platform 입력
-            System.out.println("Enter based cloud platform : ");
-            String basedCloud = scanner.nextLine();
-
-            // 입력에 해당하는 값이 cloudplatformlist 에 있다면 (유효한 입력이라면)
-            try {
-                basedCloudName = Platform.valueOf(basedCloud).name();
-                flag1 = true;
-            } catch(IllegalArgumentException e) {
-                System.out.println("Invalid input");
-            }
-        }
-        */
 
         System.out.println("Target Cloud Select");
         String targetCloudName = getCloudPlatformInput(platforms);
-        /*
-        // target Cloud Platform 입력
-        while(!flag2) {
-            // target cloud platform 입력
-            System.out.println("Enter target cloud platform : ");
-            int targetCloudNumber = scanner.nextLine();
-
-            // 올바른 입력 ( platformList index )
-            try {
-                targetCloudName = Platform.valueOf(targetCloud).name();
-                targetCloudName = platforms[targetCloudNumber].name();
-                flag2 = true;
-            } catch(IllegalArgumentException e) {
-                System.out.println("Invalid Input");
-            }
-        }
-        */
 
         if(!migrator.migrate(basedCloudName, targetCloudName))
             System.out.println("Invalid Platform");
